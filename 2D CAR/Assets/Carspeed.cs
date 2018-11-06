331a3b603fdc7551085d982cb75275c11df975e4
KimGyunYop <gyop0817@naver.com>
2018-11-07 01:50:46 +0900

    new

---
+++ 2D CAR/Assets/Carspeed.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carspeed : MonoBehaviour
+{
+
+    float speed, max_speed;
+    Vector2 direction;
+    // Use this for initialization
+    void Start()
+    {
+        speed = 0f;
+        max_speed = 0.1f;
+        direction = Vector3.up;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (speed < 0.01f) speed = 0.01f;
+            else if(speed < max_speed)speed *= 1.03f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, 0, 1);
+            //회전
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, 0, -1);//회전
+        }
+        if (Input.GetKey(KeyCode.S))
+        {//브레이크
+            speed *= 0.95f;
+        }
+
+        transform.Translate(0, speed, 0);
+    }
+}
---
+++ 2D CAR/Assets/GameDirector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour {
+
+    GameObject car, flag, distance;
+	// Use this for initialization
+	void Start () {
+        car = GameObject.Find("car");
+        flag = GameObject.Find("flag");
+        distance = GameObject.Find("Distance");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 dis = car.transform.position - flag.transform.position;
+
+        float length = dis.magnitude;
+        distance.GetComponent<Text>().text = "남은거리 : " + length.ToString("F2") + "m";
+	}
+}
